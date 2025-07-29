edd2829d1e0473f98ee99b1ac7ed4ac71ebb9726
meemmahmuda <meemmahmuda85@gmail.com>
2025-07-29 11:44:18 +0600

    up

---
+++ cypress/e2e/MyScripts/nav.cy.js
@@ -0,0 +1,34 @@
+describe('Navigation Test', () => {
+  it('should navigate to Dashboard and Cancelled pages after login', () => {
+    cy.visit('https://list.sanjoydeyreju.com/');
+
+    // Login
+    cy.get('input[name="email"]').type('admin@gmail.com');
+    cy.get('input[name="password"]').type('111');
+    cy.get('button[type="submit"]').click();
+    cy.wait(1000);
+    cy.url().should('not.include', '/login');
+
+    // Navigate to Dashboard
+    cy.contains('a', 'Dashboard').click();
+    cy.url().should('include', '/dashboard');
+    cy.wait(500);
+
+    // Navigate to Cancelled
+    cy.contains('a', 'Cancelled').click();
+    cy.url().should('include', '/cancelled');
+    cy.wait(500);
+  });
+
+  it('should redirect to login if not authenticated', () => {
+    cy.clearCookies();
+    cy.clearLocalStorage();
+
+    // Visit protected route
+    cy.visit('https://list.sanjoydeyreju.com/cancelled', { failOnStatusCode: false });
+
+    // Should be redirected to login
+    cy.url().should('eq', 'https://list.sanjoydeyreju.com/');
+  });
+  
+});
